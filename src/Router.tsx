d2bfec49458b5aa057b2fc230ae5d693c9e7d32a
felipinas <joao.felipe@neurotech.com.br>
2022-11-14 00:24:44 -0300

    feat: add route of success page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home';
 import { Checkout } from './pages/Checkout/Checkout';
+import { Success } from './pages/Success/Success';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/checkout" element={<Checkout />} />
+      <Route path="/success" element={<Success />} />
     </Routes>
   );
 }
